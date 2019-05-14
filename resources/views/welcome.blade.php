13102f9f5f222932bc84982c5aa84487a2c6a333
kunalsoni96 <39870804+kunalsoni96@users.noreply.github.com>
2019-05-14 17:09:05 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,57 +8,57 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="{{url('/')}}/public/css/app.css">
         <!-- Bootstrap Min CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/bootstrap.min.css">
         <!-- Animate CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/animate.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/animate.css">
         <!-- Meanmenu Min CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/meanmenu.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/meanmenu.css">
         <!-- Magnific Popup Min CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/magnific-popup.min.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/magnific-popup.min.css">
         <!-- Owl Carousel Min CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/owl.carousel.min.css">
         <!-- Odometer CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/odometer.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/odometer.css">
         <!-- Slick CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/slick.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/slick.css">
         <!-- Style CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/style.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/style.css">
         <!-- Responsive CSS -->
-        <link rel="stylesheet" href="{{url('/')}}/public/assets/css/responsive.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/responsive.css">
     </head>
     <body>
         <div id="root"></div>
     </body>
     <script type="text/javascript" src="{{url('/')}}/public/js/app.js"></script>
-     <script src="{{url('/')}}/public/assets/js/jquery.min.js"></script>
+     <script src="{{url('/')}}/assets/js/jquery.min.js"></script>
       <!-- Popper Min JS -->
-      <script src="{{url('/')}}/public/assets/js/popper.min.js"></script>
+      <script src="{{url('/')}}/assets/js/popper.min.js"></script>
       <!-- Bootstrap Min JS -->
-      <script src="{{url('/')}}/public/assets/js/bootstrap.min.js"></script>
+      <script src="{{url('/')}}/assets/js/bootstrap.min.js"></script>
       <!-- Meanmenu Min JS -->
-      <script src="{{url('/')}}/public/assets/js/jquery.meanmenu.min.js"></script>
+      <script src="{{url('/')}}/assets/js/jquery.meanmenu.min.js"></script>
       <!-- WOW Min JS -->
-      <script src="{{url('/')}}/public/assets/js/wow.min.js"></script>
+      <script src="{{url('/')}}/assets/js/wow.min.js"></script>
       <!-- Magnific Popup Min JS -->
-      <script src="{{url('/')}}/public/assets/js/jquery.magnific-popup.min.js"></script>
+      <script src="{{url('/')}}/assets/js/jquery.magnific-popup.min.js"></script>
       <!-- Appear Min JS -->
-      <script src="{{url('/')}}/public/assets/js/jquery.appear.js"></script>
+      <script src="{{url('/')}}/assets/js/jquery.appear.js"></script>
       <!-- Odometer Min JS -->
-      <script src="{{url('/')}}/public/assets/js/odometer.min.js"></script>
+      <script src="{{url('/')}}/assets/js/odometer.min.js"></script>
       <!-- Slick Min JS -->
-      <script src="{{url('/')}}/public/assets/js/slick.js"></script>
+      <script src="{{url('/')}}/assets/js/slick.js"></script>
       <!-- Owl Carousel Min JS -->
-      <script src="{{url('/')}}/public/assets/js/owl.carousel.min.js"></script>
+      <script src="{{url('/')}}/assets/js/owl.carousel.min.js"></script>
       <!-- Feather Icon Min JS -->
-      <script src="{{url('/')}}/public/assets/js/feather.min.js"></script>
+      <script src="{{url('/')}}/assets/js/feather.min.js"></script>
       <!-- Form Validator Min JS -->
-          <script src="{{url('/')}}/public/assets/js/form-validator.min.js"></script>
+          <script src="{{url('/')}}/assets/js/form-validator.min.js"></script>
           <!-- Contact Form Min JS -->
-          <script src="{{url('/')}}/public/assets/js/contact-form-script.js"></script>
+          <script src="{{url('/')}}/assets/js/contact-form-script.js"></script>
           <!-- StartP Map JS FILE -->
-          <script src="{{url('/')}}/public/assets/js/startp-map.js"></script>
+          <script src="{{url('/')}}/assets/js/startp-map.js"></script>
           <!-- Main JS -->
-      <script src="{{url('/')}}/public/assets/js/main.js"></script>
+      <script src="{{url('/')}}/assets/js/main.js"></script>
       
       <script>
         jQuery(document).on('ready', function() {
