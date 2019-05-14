42a31afe199e2a0a85c39176a4ee7e8dc48b4804
kunalsoni96 <39870804+kunalsoni96@users.noreply.github.com>
2019-05-14 17:27:23 +0530

    Update Services.js

--- resources/js/components/pages/Services.js
+++ resources/js/components/pages/Services.js
@@ -19,14 +19,14 @@ export default class Contact extends Component{
         </div>
       </div>
     </div>
-    <div className="shape1"><img src="public/assets/img/shape1.png" alt="shape" /></div>
-    <div className="shape2 rotateme"><img src="public/assets/img/shape2.svg" alt="shape" /></div>
-    <div className="shape3"><img src="public/assets/img/shape3.svg" alt="shape" /></div>
-    <div className="shape4"><img src="public/assets/img/shape4.svg" alt="shape" /></div>
-    <div className="shape5"><img src="public/assets/img/shape5.png" alt="shape" /></div>
-    <div className="shape6 rotateme"><img src="public/assets/img/shape4.svg" alt="shape" /></div>
-    <div className="shape7"><img src="public/assets/img/shape4.svg" alt="shape" /></div>
-    <div className="shape8 rotateme"><img src="public/assets/img/shape2.svg" alt="shape" /></div>
+    <div className="shape1"><img src="assets/img/shape1.png" alt="shape" /></div>
+    <div className="shape2 rotateme"><img src="assets/img/shape2.svg" alt="shape" /></div>
+    <div className="shape3"><img src="assets/img/shape3.svg" alt="shape" /></div>
+    <div className="shape4"><img src="assets/img/shape4.svg" alt="shape" /></div>
+    <div className="shape5"><img src="assets/img/shape5.png" alt="shape" /></div>
+    <div className="shape6 rotateme"><img src="assets/img/shape4.svg" alt="shape" /></div>
+    <div className="shape7"><img src="assets/img/shape4.svg" alt="shape" /></div>
+    <div className="shape8 rotateme"><img src="assets/img/shape2.svg" alt="shape" /></div>
   </div>
   {/* End Page Title */}
   {/* Start Services Area */}
